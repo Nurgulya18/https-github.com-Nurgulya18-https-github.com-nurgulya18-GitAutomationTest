a78585d010f98cc5fc7f08fe9ab34a5648e9b40e
nurgulya18 <nurgulya1818@gmail.com>
2023-03-15 14:14:09 -0500

    Amazon.java

---
+++ src/test/java/Amazon.java
@@ -0,0 +1,7 @@
+package test.java;
+
+public class Amazon {
+    public static void main (String[]args){
+        System.out.println("welcome to Amazon main method");
+    }
+}
---
+++ src/test/java/pages/Google.java
@@ -0,0 +1,8 @@
+package test.java.pages;
+
+public class Google {
+    public  static void main(String[]args){
+        System.out.println("Welcome to Google!");
+
+    }
+}
